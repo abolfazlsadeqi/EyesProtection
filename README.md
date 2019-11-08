3a1e2a1ebb186b3bfe5781f4e4f1664b25173416
abolfazl <abolfazlsadeqi2001@gmail.com>
2019-11-08 14:26:44 +0330

    add --dialogly -d -n options,change the variables names,change the variables to non-final,change the readme.txt change the time to seconds instead miliseconds

--- README.md
+++ README.md
@@ -1,12 +1,25 @@
 # Eyes protection
 It is just a simple software that inform you each 10 minutes that this time is to take the rest of your eyes
 
-#just for linux users
+(windows users must run via --dialogly)
 
-you can change the time by the following syntax
+#you can change the working time by the following syntax(default : 600 (10 min))
 
-syntax:
-java -jar timer.jar -t timerPerMiliSeconds
+	syntax:
+		java -jar timer.jar -t timerPerSeconds
+	example:
+		java -jar timer.jar -t 600  
 
-example:
-java -jar timer.jar -t 1000   //for each 1 second show a notification
+#you can change the number of rest parts by the following syntax (default: 10)
+
+	syntax:
+		java -jar timer.jar -n numberOfParts
+	example:
+		java -jar timer.jar -n 10
+
+#you can change the during of rest time for each part (defalut : 1sec)
+
+	syntax:
+		java -jar timer.jar -d delay
+	example:
+		java -jar timer.jar -d 1
--- timer/src/main/java/io/applications/timer/App.java
+++ timer/src/main/java/io/applications/timer/App.java
@@ -1,23 +1,51 @@
 package io.applications.timer;
 
+import javax.swing.JOptionPane;
+
 import javazoom.jl.player.Player;
 
 public class App {
 	
-	private final static String TIME_FLAG = "-t";//TIME FLAG ARGUMENT
+	private final static String WORKING_TIME_FLAG = "-t";//TIME FLAG ARGUMENT
 	private static int timeToDelay = 10 * 60 * 1000;//TIME FOR DELAY DEFAULT 10 MINUTES (10 * 60 * 1000)
 	
-	private final static int NUMBER_OF_PLAYED_SOUNDS_AFTER_DELAY = 10;
-	private final static int DELAY_TIME_FOR_EACH_SOUND = 1000;
+	private final static String NUMBER_OF_PARTS_FOR_REST_FLAG = "-n";
+	private static int numberOfPartsOfRestFlag = 10;
+	
+	private final static String REST_DEALY_FOR_EACH_PART_FLAG = "-d";
+	private static int delayForEachPartOfRestTime = 1000;
+	
+	private static final String SHOW_DIALOG_FLAG = "--dialogly";
+	private static boolean isShowDialog = false;
 	
 	private App(String[] args) {
 		try {
-			//IF WE HAVE A TIME ARGUMENT USE IT FOR TIME-TO-DELAY
+			//CONFIGURE THE ARGUMENTS
 			for(int i=0; i<args.length; i++) {
-				if(args[i].equalsIgnoreCase(TIME_FLAG)) {//IF WE HAVE THE TIME ARGUMENT
+				//SETUP THE WORKING TIME FLAG
+				if(args[i].equalsIgnoreCase(WORKING_TIME_FLAG)) {//IF WE HAVE THE TIME ARGUMENT
 					if(args.length > i+1) {//IF WE HAVE ANOTHER ONE INDEX
-						timeToDelay = Integer.parseInt(args[i+1]);//GET THE NEXT INDEX AS TIME TO DELAY
+						timeToDelay = Integer.parseInt(args[i+1]) * 1000;//GET THE NEXT INDEX AS TIME TO DELAY # PER SECOND
 					}
+				}
+				
+				//SETUP THE NUMBER OF PART OF REST FLAG
+				if(args[i].equalsIgnoreCase(NUMBER_OF_PARTS_FOR_REST_FLAG)) {
+					if(args.length > i+1) {
+						numberOfPartsOfRestFlag = Integer.parseInt(args[i+1]);
+					}
+				}
+				
+				//SETUP THE DELAY FOR EACH PART OF REST TIME
+				if(args[i].equalsIgnoreCase(REST_DEALY_FOR_EACH_PART_FLAG)) {
+					if(args.length > i+1) {
+						delayForEachPartOfRestTime = Integer.parseInt(args[i+1]) * 1000;
+					}
+				}
+				
+				//SETUP TO SHOW DIALOG FRAME
+				if(args[i].equalsIgnoreCase(SHOW_DIALOG_FLAG)) {
+					isShowDialog = true;//WHEN USE THIS FLAG USING THE DIALOG TO INFROM STARTING
 				}
 			}
 			
@@ -27,8 +55,12 @@ public class App {
 				Runtime runtime = Runtime.getRuntime();//EXECUTE THE SHOW NOTIFICATION COMMAND
 				runtime.exec("notify-send this-time-to-take-a-rest-to-your-eyes");
 				
-				for(int i=0; i<NUMBER_OF_PLAYED_SOUNDS_AFTER_DELAY; i++) {//WAIT FOR NUMBER_OF_PLAYED_SOUNDS_AFTER_DELAY * DELAY_TIME_FOR_EACH_SOUND
-					Thread.sleep(DELAY_TIME_FOR_EACH_SOUND);//WAIT FOR A TIME THEN PLAY
+				if(isShowDialog) {
+					JOptionPane.showMessageDialog(null, "this time to take a rest to your eyes");
+				}
+				
+				for(int i=0; i<numberOfPartsOfRestFlag; i++) {//WAIT FOR NUMBER_OF_PLAYED_SOUNDS_AFTER_DELAY * DELAY_TIME_FOR_EACH_SOUND
+					Thread.sleep(delayForEachPartOfRestTime);//WAIT FOR A TIME THEN PLAY
 					//START PLAYING
 					Player player = new Player(getClass().getResourceAsStream("1.mp3"));
 					player.play();
